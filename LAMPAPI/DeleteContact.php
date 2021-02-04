5131e7c34f5129b7d96296c6d59b1aa1de281b99
beccakiz <60019378+beccakiz@users.noreply.github.com>
2021-02-04 10:08:13 -0500

    Add files via upload

---
+++ LAMPAPI/DeleteContact.php
@@ -0,0 +1,45 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$ID = $inData["ID"];
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$Email = $inData["Email"];
+	$PhoneNumber = $inData["PhoneNumber"];
+	$UserId = $inData["UserId"];
+	
+	$conn = new mysqli("localhost", "Admin", "admin123", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$sql = "delete from ContactInfo (ID,FirstName,LastName,Email,PhoneNumber,UserId) VALUES (" . $ID . ",'" . $FirstName . ",'" . $LastName . ",'" . $Email . ",'" . $PhoneNumber . ",'" . $UserId . "')";
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$conn->close();
+	}
+	
+	returnWithError("");
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
