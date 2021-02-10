b33c82ba044fccaae2ef54b71fd14fdcefaae6e5
TheRider98 <61335426+TheRider98@users.noreply.github.com>
2021-02-10 17:21:52 -0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -198,7 +198,7 @@ function SearchContacts()
 				var jsonObject = JSON.parse( xhr.responseText );
 
 				contactList = jsonObject.results
-				newList = JSON.stringify(contactList.results);
+				newList = JSON.stringify(contactList["FirstName"]+contactList["LastName"]+contactList["Email"]+contactList["Number"]);
 						newList += "<br />\r\n";
 
 
