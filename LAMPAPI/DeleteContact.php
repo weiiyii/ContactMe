76a411431ff09a3311d0f9e5c1b1f3e5f1e74bd8
TheRider98 <61335426+TheRider98@users.noreply.github.com>
2021-02-13 15:41:13 -0500

    Update DeleteContact.php

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,30 +1,25 @@
 <?php
 	$inData = getRequestInfo();
-	
+
 	$ID = $inData["ID"];
-	$FirstName = $inData["FirstName"];
-	$LastName = $inData["LastName"];
-	$Email = $inData["Email"];
-	$PhoneNumber = $inData["PhoneNumber"];
-	$UserID = $inData["UserID"];
-	
+
 	$conn = new mysqli("localhost", "Admin", "admin123", "COP4331");
-	if ($conn->connect_error) 
+	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
-	} 
+	}
 	else
 	{
-		$sql = "DELETE FROM ContactInfo (ID, FirstName, LastName, Email, PhoneNumber, UserID) VALUES (" . $ID . ",'" . $FirstName . ",'" . $LastName . ",'" . $Email . ",'" . $PhoneNumber . ",'" . $UserID . "')";
+		$sql = "DELETE FROM ContactInfo WHERE ID = $ID";
 		if( $result = $conn->query($sql) != TRUE )
 		{
 			returnWithError( $conn->error );
 		}
 		$conn->close();
 	}
-	
+
 	returnWithError("");
-	
+
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
@@ -35,11 +30,11 @@
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
+
 	function returnWithError( $err )
 	{
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
+
 ?>
