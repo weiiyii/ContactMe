2d8b41d941c433427d3778a0cc5ebaf97dde712c
TheRider98 <61335426+TheRider98@users.noreply.github.com>
2021-02-13 16:05:46 -0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -196,9 +196,9 @@ function SearchContacts()
 			{
 				document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
 				var jsonObject = JSON.parse( xhr.responseText );
-				contactList += `<tr>\n<td>${jsonObject.results[0]}</td>`;
-				for( var i=1; i<jsonObject.results.length; i++ ){
-					if(i%4==0){
+				contactList += `<tr>\n<td>${jsonObject.results[1]}</td>`;
+				for( var i=2; i<jsonObject.results.length; i++ ){
+					if(i%5==0){
 						contactList += "</tr>\r\n";
 					}
 				contactList += `<td>${jsonObject.results[i]}</td>`;
