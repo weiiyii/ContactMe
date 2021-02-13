74e16de2acdf96ed64384c36ebf86b19db82def9
TheRider98 <61335426+TheRider98@users.noreply.github.com>
2021-02-12 21:43:31 -0500

    update search

--- js/code.js
+++ js/code.js
@@ -196,9 +196,12 @@ function SearchContacts()
 			{
 				document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
 				var jsonObject = JSON.parse( xhr.responseText );
-
-				for( var i=0; i<jsonObject.results.length; i++ ){
-				contactList += jsonObject.results[i];
+				contactList += "<tr>\n<td>"+jsonObject.results[0]+"</td>";
+				for( var i=1; i<jsonObject.results.length; i++ ){
+				contactList += "<td>"+jsonObject.results[i]+"</td>";
+				if(i%4==0){
+					contactList += "</tr>\r\n";
+				}
 				if(i<jsonObject.results.length - 1){
 					contactList += "<br />\r\n";
 				}
