c39cc831fe9f5fcda2a76afdeb161d9b45211cee
TheRider98 <61335426+TheRider98@users.noreply.github.com>
2021-02-14 15:39:43 -0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -207,7 +207,7 @@ function SearchContacts()
 	document.getElementById("contactSearchResult").innerHTML = "";
 	var srchType = document.getElementById("SearchT").value;
 
-	var contactList = "<thead><tr>\n<th>First</th>\n<th>Last</th>\n<th>Email</th>\n<th>Number</th>\n<th>Date Created</th>\n<th>Delete</th>\n</tr>\r\n";
+	var contactList = "<thead><tr>\n<th>First</th>\n<th>Last</th>\n<th>Email</th>\n<th>Number</th>\n<th>Date Created</th>\n<th>Delete</th>\n<th>Update</th>\n</tr>\r\n";
 
 	var jsonPayload = '{"search" : "' + srch + '","searchT" : "' + srchType + '","userId" : ' + userId + '}';
 	var url = urlBase + '/SearchContacts.' + extension;
@@ -227,10 +227,13 @@ function SearchContacts()
 				for( var i=1; i<jsonObject.results.length; i++ ){
 					if((i+1)%6==0){
 						contactList += `<td><button type="button" id="deleteButton" class="btn btn-outline-primary" style="border: 2px solid; font-weight:500" onclick="doDelete(${jsonObject.results[i]});"> Delete </button></td>`;
-						contactList += "</tr>\r\n";
 					}
 					else if((i+1)%5==0){
 							contactList += `<td>${jsonObject.results[i]}</td>`;
+					}
+					else if((i+1)%7==0){
+						contactList += `<td><button type="button" id="updateButton" class="btn btn-outline-primary" style="border: 2px solid; font-weight:500" onclick="updateContact(${jsonObject.results[i-1]});"> Update </button></td>`;
+							contactList += "</tr>\r\n";
 					}
 					else{
 						contactList += `<td contenteditable='true'>${jsonObject.results[i]}</td>`;
