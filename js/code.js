cf0b75431208e8cef5bce247f4e1ce64e7f90113
TheRider98 <61335426+TheRider98@users.noreply.github.com>
2021-02-15 14:40:20 -0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -154,7 +154,6 @@ function doDelete(row){
 		{
 			if (this.readyState == 4 && this.status == 200)
 			{
-				document.getElementById("contactDelUpResult").innerHTML = "Contact has been Deleted";
 				SearchContacts();
 			}
 		};
