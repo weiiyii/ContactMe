9d8fd30c5dc6684fda18606fce24523a180d0146
weiiyii <cwy.sophiechen@knights.ucf.edu>
2021-02-14 20:37:53 -0500

    .

--- js/code.js
+++ js/code.js
@@ -255,6 +255,8 @@ function SearchContacts()
 					// update
 					// contactList += `<td >${jsonObject.results[i]}</td>\n</tr>\n`;
 
+					row_cnt++;
+
 					// if((i+1)%6==0){
 					// 	contactList += `<td><button type="button" id="deleteButton" class="btn btn-outline-primary" style="border: 2px solid; font-weight:500" onclick="doDelete(${jsonObject.results[i]});"> Delete </button></td>`;
 					// 	contactList += `<td><button type="button" id="updateButton" class="btn btn-outline-primary" style="border: 2px solid; font-weight:500" onclick="updateContact(${jsonObject.results[i]},${(((i+1)/6)-1)});"> Update </button></td>`;
